85d6801dc660621a0f196863140cf6920243d412
Keith Schincke <keith.schincke@gmail.com>
2026-02-04 11:56:50 -0600

    Upate packages to add which. rohc's autogen uses which to find aclocal

--- docker/scripts/install_rohc_dependencies.sh
+++ docker/scripts/install_rohc_dependencies.sh
@@ -35,7 +35,7 @@ main() {
         fi
     elif [[ "$ID" == "fedora" || "$ID" == "rhel" ]]; then
         if [[ "$mode" == "all" || "$mode" == "build" ]]; then
-            dnf -y install curl ca-certificates gcc-c++ make xz autoconf automake libtool libpcap-devel libcmocka-devel
+            dnf -y install curl ca-certificates gcc-c++ make xz autoconf automake libtool libpcap-devel libcmocka-devel which
         fi
         if [[ "$mode" == "all" || "$mode" == "run" ]]; then
             : # noop, nothing needed

7a871296c82849865cbe9fd436c8fcaf91048bc4
Keith Schincke <keith.schincke@gmail.com>
2026-02-04 10:58:22 -0600

    Docker compose updates

--- docker/scripts/install_rohc_dependencies.sh
+++ docker/scripts/install_rohc_dependencies.sh
@@ -33,6 +33,13 @@ main() {
         if [[ "$mode" == "all" || "$mode" == "run" ]]; then
             : # noop, nothing needed
         fi
+    elif [[ "$ID" == "fedora" || "$ID" == "rhel" ]]; then
+        if [[ "$mode" == "all" || "$mode" == "build" ]]; then
+            dnf -y install curl ca-certificates gcc-c++ make xz autoconf automake libtool libpcap-devel cmocka-devel
+        fi
+        if [[ "$mode" == "all" || "$mode" == "run" ]]; then
+            : # noop, nothing needed
+        fi
     else
         echo "OS $ID not supported"
         exit 1
